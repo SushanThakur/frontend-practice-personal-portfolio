e9b96befc5236630282dc5df9060824ed48e0253
SushanThakur <mail@sushant.name.np>
2024-10-17 13:40:54 +0545

    frontend complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,29 @@
 const menuIcon = document.getElementById('menu');
 const closeIcon = document.getElementById('close');
 const mobileNav = document.getElementById('mobile-ul');
+const mobileExitDiv = document.getElementById('mobile-nav-exit');
 
 // Add event listener to open the menu
 menuIcon.addEventListener('click', () => {
-    mobileNav.classList.add('visible');  // Show the mobile menu
-    menuIcon.style.display = 'none';     // Hide the menu icon
-    closeIcon.style.display = 'block';   // Show the close icon
+    mobileNav.classList.add('visible');        // Show the mobile menu
+    menuIcon.style.display = 'none';           // Hide the menu icon
+    closeIcon.style.display = 'block';         // Show the close icon
+    mobileExitDiv.classList.add('visible');    // Hide div
 });
 
 // Add event listener to close the menu
 closeIcon.addEventListener('click', () => {
-    mobileNav.classList.remove('visible'); // Hide the mobile menu
-    menuIcon.style.display = 'block';      // Show the menu icon
-    closeIcon.style.display = 'none';      // Hide the close icon
+    mobileNav.classList.remove('visible');       // Hide the mobile menu
+    menuIcon.style.display = 'block';            // Show the menu icon
+    closeIcon.style.display = 'none';            // Hide the close icon
+    mobileExitDiv.classList.remove('visible');
+});
+
+mobileExitDiv.addEventListener('click', () => {
+    mobileNav.classList.remove('visible');       // Hide the mobile menu
+    menuIcon.style.display = 'block';            // Show the menu icon
+    closeIcon.style.display = 'none';            // Hide the close icon
+    mobileExitDiv.classList.remove('visible');   // Hide div
 });
 
 
